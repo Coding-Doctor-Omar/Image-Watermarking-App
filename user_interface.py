5e0b6b390ce046544be11d0209b19a5dc909b1ee
Omar <etrius460@gmail.com>
2025-05-09 14:17:07 +0300

    Fix image dimension bug

--- user_interface.py
+++ user_interface.py
@@ -184,9 +184,9 @@ class UI:
             target_height = self.preview_section.winfo_height()
             target_width = (target_height * image_width) / image_height
 
-        resized_image = self.image
-
-        resized_image.thumbnail((target_width, target_height))
+        print("Original size:", self.image.size)
+        resized_image = self.image.resize((int(target_width), int(target_height)), Image.Resampling.LANCZOS)
+        print("Resized size:", resized_image.size)
 
         image_tk = ImageTk.PhotoImage(resized_image)
         self.image_preview = image_tk
@@ -223,9 +223,7 @@ class UI:
                 target_height = self.preview_section.winfo_height()
                 target_width = (target_height * image_width) / image_height
 
-            resized_image = self.processed_image
-
-            resized_image.thumbnail((target_width, target_height))
+            resized_image = self.processed_image.resize((int(target_width), int(target_height)), Image.Resampling.LANCZOS)
 
             image_tk = ImageTk.PhotoImage(resized_image)
             self.image_preview = image_tk
